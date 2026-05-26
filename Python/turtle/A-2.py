16e1a432f930dec290f06f8e4a946967a9bf0043
VarunHQ <varunsathish15@gmail.com>
2026-05-26 17:26:48 +0530

    A-2 Python

--- Python/turtle/A-2.py
+++ Python/turtle/A-2.py
@@ -6,10 +6,6 @@ turtle.Screen().bgcolor("yellow")
 board = turtle.Turtle()
 
 
-board.forward(100)
-
-board.left(120)
-board.forward(100)
 
 
 board.forward(100)
@@ -20,9 +16,8 @@ board.left(90)
 
 board.forward(100)
 board.left(90)
-
 board.forward(100)
-board.left(90)
+
 
 turtle.done()
 
