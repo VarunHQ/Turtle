647887eb62d9401e396e77e39f17b3a2d90d0622
VarunHQ <varunsathish15@gmail.com>
2026-05-26 21:18:17 +0530

    A-1 Python Conditions

---
+++ Python/Conditional Statements and Lists/A-1.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number to check:"))
+print(f"The number to be checked is {number}")
+
+if number>0:
+   print("It is a positive number")
+elif number<0:
+    print("It is a negative number")
+else:
+    print("It is Neutral")
