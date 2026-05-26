93034896e7250d6d4c79562a415233788a11a282
VarunHQ <varunsathish15@gmail.com>
2026-05-26 14:57:03 +0530

    A-2 turtle

---
+++ Python/turtle/A-2.py
@@ -0,0 +1,28 @@
+import turtle
+
+turtle.Screen().bgcolor("yellow")
+
+
+board = turtle.Turtle()
+
+
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+turtle.done()
+
