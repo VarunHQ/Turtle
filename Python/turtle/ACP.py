0b618c529d5df9a689ca174575a813e536a8b3ec
VarunHQ <varunsathish15@gmail.com>
2026-05-26 17:29:39 +0530

    ACP using Turtle

---
+++ Python/turtle/ACP.py
@@ -0,0 +1,26 @@
+import turtle
+
+
+
+turtle.title("A simple square using Python Turtle Library!")
+
+turtle.Screen().bgcolor("lightblue")
+
+board = turtle.Turtle()
+
+
+screen = turtle.Screen()
+screen.setup(400, 300)
+
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+board.forward(100)
+
+turtle.done() 
