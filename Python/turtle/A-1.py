132f485bd19feaa39e0e627d227d88df56aca705
VarunHQ <varunsathish15@gmail.com>
2026-05-26 14:47:53 +0530

    A-1 Python

---
+++ Python/turtle/A-1.py
@@ -0,0 +1,12 @@
+import turtle 
+import tkinter as tk
+
+
+turtle.Screen().bgcolor("lightblue")
+
+
+screen = turtle.Screen()
+screen.setup(400, 300)
+
+turtle.title("Welcome to Python Turtle Library!")
+turtle.done() 
